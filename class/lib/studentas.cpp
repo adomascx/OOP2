c1a1939cce9bbfbc6683a49d6d2ce02ae34c660a
adomas.wav <147273512+adomascx@users.noreply.github.com>
2025-05-13 22:40:44 +0300

    Pataisyti move konstruktoriu

--- class/lib/studentas.cpp
+++ class/lib/studentas.cpp
@@ -53,9 +53,9 @@ studentas::studentas(studentas &&other) noexcept
     : var(move(other.var)),
       pav(move(other.pav)),
       paz(move(other.paz)),
-      egz(other.egz),
-      galutinisVid(other.galutinisVid),
-      galutinisMed(other.galutinisMed) { other.~studentas(); }
+      egz(move(other.egz)),
+      galutinisVid(move(other.galutinisVid)),
+      galutinisMed(move(other.galutinisMed)) { other.~studentas(); }
 
 // copy operatorius
 studentas &studentas::operator=(const studentas &other)
